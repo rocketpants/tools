56b79473c6608689a64a71db5dc10248ebfe3255
Zoee Silcock <mrzoee@gmail.com>
2016-09-22 11:56:23 +0200

    Adapted a screenshot script found online to our needs

---
+++ sreenshot/Editor/ScreenshotEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Screenshot))]
+public class ScreenshotEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        Screenshot subject = (Screenshot)target;
+        if (GUILayout.Button("Take screenshot")) {
+            subject.TakeScreenshot();
+        }
+    }
+}
